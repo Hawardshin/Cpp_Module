1afe3db49fae61f2835ff072111fe39518598db6
wnddms12345 <wnddms12345@naver.com>
2023-02-10 19:10:55 +0900

    doing...
    now doing

---
+++ CPP_Module_03/ex01/ScavTrap.cpp
@@ -0,0 +1,11 @@
+# include "ScavTrap.hpp
+
+ScavTrap::ScavTrap() {
+
+}
+
+ScavTrap::ScavTrap(const ScavTrap &obj) {}
+
+~ScavTrap::ScavTrap() {}
+
+ScavTrap& ScavTrap::operator=(const ScavTrap& obj){}
