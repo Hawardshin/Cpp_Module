06917cd943f99c5fc2ee0316c8a8e368e0a0352b
wnddms12345 <wnddms12345@naver.com>
2023-03-09 20:41:00 +0900

    Base class doing
    Q : is this ok? in base cpp return A,B,C?

--- CPP_Module_06/ex02/Base.hpp
+++ CPP_Module_06/ex02/Base.hpp
@@ -1,6 +1,12 @@
 #pragma once
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 
 class Base{
   virtual ~Base();
 }
+
+Base* generate(void);
+void  identify(Base* p);
+void  identify(Base& p);
--- CPP_Module_06/ex02/main.cpp
+++ CPP_Module_06/ex02/main.cpp
@@ -1,16 +1,8 @@
+#include "Base.hpp"
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
-Base * generate(void){
 
-}
-void identify(Base* p){
-
-}
-
-void identify(Base& p){
-
-}
 int main(){
 
 }
