e6644e5e6ee6fd81d285b3a21c5636c4b697c59f
wnddms12345 <wnddms12345@naver.com>
2023-03-09 20:01:06 +0900

    ex01 main edit done
    main edit add more test

--- CPP_Module_06/ex01/main.cpp
+++ CPP_Module_06/ex01/main.cpp
@@ -8,7 +8,8 @@ int main(){
   Data *test2;
   std::cout << "before : " << &d << "\n";
   test1 = Serializer::serialize(&d);
+  std::cout << test1 << "\n";
   test2 =  Serializer::deserialize(test1);
   std::cout << "after  : "  << test2 << "\n";
-
+  std::cout << test2->a <<" " <<test2->b << "\n";
 }
