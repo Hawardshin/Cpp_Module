9454eb5883cbeb48cb7114373d817c79ec3caddf
wnddms12345 <wnddms12345@naver.com>
2023-03-24 21:41:47 +0900

    Cpp 07 init
    init cpp

---
+++ CPP_Module_07/ex00/whatever.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+class whatever
+{
+public:
+  whatever();
+  explicit whatever();
+  whatever(const whatever& obj );
+  ~whatever();
+  whatever& operator=(const whatever& obj );
+
+private:
+
+};
